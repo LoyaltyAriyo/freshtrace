e91894eacab3adc2e4c124764d552d787d4a0821
Amirhossein Mohammadi <amirhossein1384m@gmail.com>
2026-03-19 22:40:15 -0400

    Remove legacy root app files after client/server split

--- app/scan/page.tsx
+++
@@ -1,5 +0,0 @@
-import { ReceiptUploadForm } from "@/components/receipt-upload-form"
-
-export default function ScanPage() {
-  return <ReceiptUploadForm />
-}
--- components/ui/sonner.tsx
+++
@@ -1,9 +0,0 @@
-"use client"
-
-import { Toaster as Sonner, type ToasterProps } from "sonner"
-
-const Toaster = ({ ...props }: ToasterProps) => {
-  return <Sonner {...props} />
-}
-
-export { Toaster }
